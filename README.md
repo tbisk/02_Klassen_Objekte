0ceac01efd5619ea916211b7a91735315cd31bb9
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-08-24 09:55:52 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# 02_Klassen_Objekte
+
+### Klassen & Objekte in Java
---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    
+    public static void main(String[] args) {
+        // 
+    }
+
+    public static void ausgabe(String outputStr) {
+      System.out.println(outputStr);  
+    }
+
+
+}
