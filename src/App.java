560641c881ed1e83a112d36897e191a956e59539
Peter <schmidt.multimedia@gmail.com>
2021-08-25 10:47:22 +0200

    07 / custom constructor | furColor ++ | age ++

--- src/App.java
+++ src/App.java
@@ -2,18 +2,19 @@ public class App {
     
     public static void main(String[] args) {
         
-      Cat cat1  = new Cat(); 
-      cat1.name = "Grizabella";
+      Cat cat1  = new Cat("Grizabella", "white", 29);
       ausgabe("RAM-Adresse: " + cat1.tellYourAddress());
       ausgabe("Name: " + cat1.tellYourName());
+      // Fellfarbe --> tellYourFurColor()
+      // Alter --> tellYourAge()
+
     
       ausgabe("--------------------");
 
-      Cat cat2  = new Cat(); 
-      cat2.name = "Alonzo";
+      Cat cat2  = new Cat("Alonzo", "grey", 35);
       ausgabe("RAM-Adresse: " + cat2.tellYourAddress());
       ausgabe("Name: " + cat2.tellYourName());
-      ausgabe(cat2.name);
+     
    
     }
 
--- src/Cat.java
+++ src/Cat.java
@@ -1,7 +1,16 @@
 public class Cat {
     
     // Attribut | Field | Property
-    public String name = "nobody";
+    public String name;
+    public String furColor; 
+    public int age;
+
+    Cat(String name, String furColor, int age){
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
 
     public Cat tellYourAddress(){
        return this;
@@ -9,6 +18,10 @@ public class Cat {
 
     public String tellYourName(){
         return this.name;
+    }
+
+    public void changeYourName(String name){
+        this.name = name;
     }
     
 }
