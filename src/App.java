46f8f619cd7ecec978b5805b098d7710acfd1066
Peter <schmidt.multimedia@gmail.com>
2021-08-25 08:38:40 +0200

    05 / 2 Instanzen

--- src/App.java
+++ src/App.java
@@ -2,10 +2,16 @@ public class App {
     
     public static void main(String[] args) {
         
-    Cat cat  = new Cat(); 
-    ausgabe("Blick von aussen: " + cat);
-    ausgabe("Blick von innen: " + cat.tellYourAddress());
+    Cat cat1  = new Cat(); 
+    ausgabe("Blick von aussen: " + cat1);
+    ausgabe("Blick von innen: " + cat1.tellYourAddress());
     
+    ausgabe("--------------------");
+
+    Cat cat2  = new Cat(); 
+    ausgabe("Blick von aussen: " + cat2);
+    ausgabe("Blick von innen: " + cat2.tellYourAddress());
+
     }
 
     public static void ausgabe(String outputStr) {
